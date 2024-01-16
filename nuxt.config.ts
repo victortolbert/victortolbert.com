ce7bd08fc1d10543035af1760d95f41f6f383c3c
Victor Tolbert <victor.tolbert@gmail.com>
2024-01-16 18:50:45 -0500

    wip

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,26 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: 'en',
+        class: 'overflow-y-scroll',
+      },
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      title: 'Victor Tolbert',
+      titleTemplate: '%s - victortolbert.com',
+      link: [
+        { rel: 'icon', href: '/favicon.svg', sizes: 'any' },
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon-dark.svg' },
+      ],
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      ],
+    },
+  },
+
   devServer: {
     port: 8589,
   },
