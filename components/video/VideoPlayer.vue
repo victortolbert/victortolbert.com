882e5c51ecb8fb68981f58f9b58cde421f77e5f8
Victor <victor.tolbert@gmail.com>
2024-01-28 19:00:06 -0500

    wip

---
+++ components/video/VideoPlayer.vue
@@ -0,0 +1,32 @@
+<script setup>
+import videojs from 'video.js'
+
+defineProps({
+  options: {
+    type: Object,
+    default() {
+      return {}
+    },
+  },
+})
+
+const player = ref()
+const videoPlayer = ref(null)
+
+onMounted(() => {
+  player.value = videojs(videoPlayer.value, props.options, () => {
+    player.value.log('onPlayerReady', this)
+  })
+})
+
+onBeforeUnmount(() => {
+  if (player.value)
+    player.value.dispose()
+})
+</script>
+
+<template>
+  <div>
+    <video ref="videoPlayer" class="video-js" />
+  </div>
+</template>
