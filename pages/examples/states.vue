1310714d954135ff4af9e8555da365cfd34bc7f4
Victor Tolbert <victor.tolbert@gmail.com>
2024-02-02 16:10:42 -0500

    wip

---
+++ pages/examples/states.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { useQuery } from '@tanstack/vue-query'
+
+async function fetcher() {
+  return await fetch('https://api.victortolbert.com/states').then(response =>
+    response.json(),
+  )
+}
+
+const { data, suspense } = useQuery({ queryKey: ['test'], queryFn: fetcher })
+
+await suspense()
+</script>
+
+<template>
+  <div>{{ data }}</div>
+</template>
