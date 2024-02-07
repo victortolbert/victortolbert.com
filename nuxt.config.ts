9b23f06c56e61d34bdeaa8ab16b1e5b83faf9769
Victor Tolbert <victor.tolbert@gmail.com>
2024-02-07 18:08:01 -0500

    wip

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -442,11 +442,11 @@ export default defineNuxtConfig({
 
   nitro: {
     routeRules: {
-      '/**': {
-        headers: {
-          'Cross-Origin-Embedder-Policy': 'require-corp',
-          'Cross-Origin-Opener-Policy': 'same-origin',
-        },
+      // '/**': {
+      //   headers: {
+      //     'Cross-Origin-Embedder-Policy': 'require-corp',
+      //     'Cross-Origin-Opener-Policy': 'same-origin',
+      //   },
       },
     },
   },
