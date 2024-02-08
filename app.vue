5601e2d777a38d5f7662df563abf5b8dc8bb4fed
Victor Tolbert <victor.tolbert@gmail.com>
2024-02-07 20:13:34 -0500

    wip

--- app.vue
+++ app.vue
@@ -1,7 +1,7 @@
 <script setup>
 useHead({
   bodyAttrs: {
-    class: 'antialiased font-sans bg-gray-50 dark:bg-gray-950',
+    class: 'antialiased font-mono bg-gray-50 dark:bg-gray-950',
   },
 })
 
